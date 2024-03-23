87e301373f6283c4986922e7199f01c631167203
roman <pvp380@gmail.com>
2024-03-23 15:24:55 +0500

    #9 Добавил камеру в реальном времени и открытие шлагбаума

--- str.py
+++ str.py
@@ -1,22 +1,34 @@
 import tempfile
 import cv2
+import subprocess
 import pandas as pd
 import streamlit as st
 from main import load_model, load_video, detect_cars, recognize_license_plate
 import time
-import re
 from src.config.config import get_cfg_defaults
-from src.tools.utils import decode_function, BeamDecoder
 
 cfg = get_cfg_defaults()
+df = pd.read_csv('allowed_numbers.csv')
+last_call_time = 0
+last_image = None
+
 
 st.title('Автоматическое распознавание номерных знаков')
 
-page = st.sidebar.selectbox("Выберите страницу", ["Главная", "Детекция по видео", "Детекция по фото"])
+page = st.sidebar.selectbox("Выберите страницу", ["Главная", "Детекция по видео", "Детекция по фото", "Детекция с камеры"])
+
+
+def call():
+    command = ['adb', 'shell', 'am', 'start', '-a', 'android.intent.action.CALL', '-d', 'tel:89631333413']
+    subprocess.run(command, check=True)
+
 
 if page == "Главная":
     st.header("Главная страница")
     st.write("Выберите тип детекции, используя боковую панель.")
+
+    if st.button("Открыть шлакбаум"):
+        call()
 
 elif page == "Детекция по видео":
     st.header("Детекция по видео")
@@ -95,3 +107,55 @@ elif page == "Детекция по фото":
                 _, license_plate = result
                 if license_plate is not None:
                     st.image(car_area, caption=f'Processed Image with license plate: {license_plate}', use_column_width=True)
+
+elif page == "Детекция с камеры":
+    st.header("Детекция с камеры")
+    stop_button = st.button("Стоп")
+
+    rtsp_url = st.text_input("Введите адрес RTSP")
+
+    if rtsp_url:
+        cap = cv2.VideoCapture(rtsp_url)
+        model, coco_model, lpr_model, names = load_model()
+        idx = 0
+        frame_counter = 0
+
+        while cap.isOpened() and not stop_button:
+            success, im0 = cap.read()
+            if not success:
+                st.write("Обработка видео завершена.")
+                break
+
+            # Обрабатываем только каждый четвертый кадр
+            if frame_counter % 4 == 0:
+                vehicle_bounding_boxes = detect_cars(im0, coco_model)
+
+                for box in vehicle_bounding_boxes:
+                    x1, y1, x2, y2, track_id, score = box
+                    # Crop the car area
+                    car_area = im0[int(y1):int(y2), int(x1):int(x2)]
+                    result = recognize_license_plate(car_area, model, lpr_model, names, idx)
+
+                    if result is not None:
+                        car_image, license_plate = result
+                        if car_image is not None:
+                            if last_image is not None:
+                                del last_image
+                            last_image = car_image
+                            st.image(last_image, caption=f'Processed Frame with license plate: {license_plate}',
+                                     use_column_width=True)
+
+                            # Если номер есть в списке разрешенных, сделайте звонок
+                            if license_plate in df['license_plate'].values:
+                                current_time = time.time()
+                                if current_time - last_call_time >= 60:  # 60 seconds = 1 minute
+                                    call()
+                                    last_call_time = current_time
+
+            frame_counter += 1
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
